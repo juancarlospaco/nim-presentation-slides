b3cf3b5ca639f2402aec1501d08c4e41de2fe547
juancarlospaco <juancarlospaco@gmail.com>
2018-07-10 17:26:57 -0300

    Agrega One-Liners re locas

--- ejemplos/avanzado/one-liners/lee_archivo.nim
+++ ejemplos/avanzado/one-liners/lee_archivo.nim
@@ -1,3 +1,3 @@
-import sequtils
+import sequtils                    # Tambien existe writeFile()
 echo readFile("./README.txt")      # Abre archivo, lee archivo, cierra archivo, devuelve string.
 echo to_seq(lines("./README.txt")) # Abre archivo, lee archivo por lineas, cierra archivo, devuelve lista de strings.
