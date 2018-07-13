945e43d638f0a4642665902a748f8861df2b307d
juancarlospaco <juancarlospaco@gmail.com>
2018-07-13 07:38:17 -0300

    Add oop sample

--- ejemplos/basico/object/gato.nim
+++ ejemplos/basico/object/gato.nim
@@ -6,28 +6,31 @@ type
     pulgas: bool
     sexo: char
 
-proc purr*(gato: var Gato) =
-  gato.comida -= 1
+method purr*(this: var Gato) =
+  dec this.comida
   echo "Purr"
 
-proc alimentarlo*(gato: var Gato) =
-  gato.comida += 1
+method alimentarlo*(this: var Gato) =
+  inc this.comida
   echo "Nom nom"
 
-proc lavarlo*(gato: var Gato) =
-  gato.pulgas = false
-  echo gato.pulgas
+method lavarlo*(this: var Gato) =
+  this.pulgas = false
+  echo this.pulgas
+
+method meow*(this: Gato) = echo "Meow"
 
 
 
-var gatito = Gato(nombre : "Zoe",
+var gatito = Gato(nombre :  "Zoe",
                   especie : "Agora",
-                  comida: 50,
-                  pulgas: false,
-                  sexo: 'f')
+                  comida:   50,
+                  pulgas:   false,
+                  sexo:     'f')
 
 
 
+assert gatito is Gato
 assert(not gatito.pulgas)
 
 if gatito.comida < 75:
@@ -39,8 +42,9 @@ for i in 0..9:
 
 gatito.pulgas = true
 gatito.lavarlo()
+gatito.meow()
 
 
-# range[0..100] No permite integers > 100
+# # range[0..100] No permite integers > 100
 # for i in 0..99:
 #   gatito.alimentarlo()
