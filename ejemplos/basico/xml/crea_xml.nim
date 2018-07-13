7b3d71d81c957e01a06c217b4e3aad51fd38882f
juancarlospaco <juancarlospaco@gmail.com>
2018-07-13 06:08:39 -0300

    Add XML sample

---
+++ ejemplos/basico/xml/crea_xml.nim
@@ -0,0 +1,5 @@
+import xmltree, strtabs
+
+echo <>a(href="http://nim-lang.org", title="Nim is Awesome", alt="Nim Rules", target="_blank", newText("Nim"))
+
+echo <>svg(title="Desafio", newText("Quien se anima hacer un SVG ?."))
