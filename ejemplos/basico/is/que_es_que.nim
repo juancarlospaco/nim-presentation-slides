0c0295c23e1777c8cb0127e4be6f4141a5d32c07
juancarlospaco <juancarlospaco@gmail.com>
2018-07-12 22:12:40 -0300

    Add more samples

---
+++ ejemplos/basico/is/que_es_que.nim
@@ -0,0 +1,8 @@
+type Gato = object
+var grumpy_cat: Gato
+
+#                           COSA is TIPO
+echo "'a' is char? ",        'a' is char
+echo "'a' is string? ",      'a' is string
+echo "42 is string? ",        42 is string
+echo "grumpy_cat is Gato? ", grumpy_cat is Gato
