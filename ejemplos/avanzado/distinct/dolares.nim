c3450c18f87e73be19a738468e00dd5ee72de56b
juancarlospaco <juancarlospaco@gmail.com>
2018-07-13 05:48:02 -0300

    Tiny fix on comment

--- ejemplos/avanzado/distinct/dolares.nim
+++ ejemplos/avanzado/distinct/dolares.nim
@@ -4,7 +4,6 @@ type
 
 var ahorros = 10.Dolares
 
-ahorros = 25.Dolares  # Funciona bien.
-# ahorros = 25          # Error, es int no Dolares. Proba Descomentar esta linea.
-
-echo ahorros
+ahorros = 25.Dolares   # Funciona bien.
+ahorros = Dolares(50)  # Funciona bien.
+#ahorros = 25          # Error, es int no Dolares. Proba Descomentar esta linea.
