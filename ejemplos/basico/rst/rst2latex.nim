bb3101919da55849243001b603a8fc037b002aea
juancarlospaco <juancarlospaco@gmail.com>
2018-09-19 16:04:32 -0300

    Add RST example

--- ejemplos/basico/rst/rst2latex.nim
+++ ejemplos/basico/rst/rst2latex.nim
@@ -3,8 +3,8 @@ import packages/docutils/rstgen, packages/docutils/rst
 var
   option: bool
   rst2latex: RstGenerator
-  generatedHTML = ""
+  latext = ""
 
 rst2latex.initRstGenerator(outLatex, defaultConfig(), "", {})
-rst2latex.renderRstToOut(rstParse("*Hello* **world**", "", 1, 1, option, {}), generatedHTML)
-echo generatedHTML
+rst2latex.renderRstToOut(rstParse("*Hello* **world**", "", 1, 1, option, {}), latext)
+echo latext
