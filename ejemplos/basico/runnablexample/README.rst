81a0274cbb883962de235d815fa63a711576bd0f
juancarlospaco <juancarlospaco@gmail.com>
2018-09-29 03:18:00 -0300

    Add RunnableExamples

---
+++ ejemplos/basico/runnablexample/README.rst
@@ -0,0 +1,6 @@
+
+This is a Runnable Example for Self-Documentation purposes.
+
+RunnableExamples wont run with ``nim c -r`` or similar.
+
+RunnableExamples only run with ``nim doc`` and generate HTML Docs.
---
+++ ejemplos/basico/runnablexample/runnablexample.nim
@@ -0,0 +1,12 @@
+## * This is a **Runnable Example** for *Self* ``Documentation`` purposes.
+##   - Nested Bullets.
+##
+## > `Nim <https://nim-lang.org>`_ is Awesome
+##
+## .. code-block:: nim
+##
+##  echo "This is a block of example code"
+##
+## .. image:: https://source.unsplash.com/collection/139386/400x400
+runnableExamples:
+  echo "This is a Runnable Example for Self-Documentation purposes."
