e9b779bcdfe1fed4622a0cd7148d46af40e77eed
juancarlospaco <juancarlospaco@gmail.com>
2018-09-30 00:18:17 -0300

    Add string based macro example

--- ejemplos/avanzado/macros/macro_from_string.nim
+++ ejemplos/avanzado/macros/macro_from_string.nim
@@ -1,8 +1,7 @@
 import macros
 
 macro generate_hello(): typed =
-  parseStmt """proc hello() =
-  echo "hi" """
+  parseStmt """proc hello() = echo "hi" """
 
 generate_hello()  # Generate a proc via Macros.
 hello()
