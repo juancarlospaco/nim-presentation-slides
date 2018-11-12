e723e5ddb8a6c83497d280a5e04ec1705be44fd8
juancarlospaco <juancarlospaco@gmail.com>
2018-11-12 00:44:37 -0300

    Add GC & RAM functions

---
+++ ejemplos/basico/ram_info/ram.nim
@@ -0,0 +1,3 @@
+echo getTotalMem()    # Total managed  RAM by GC, used + free, bytes.
+echo getOccupiedMem() # Total reserved RAM by GC, used by objects, bytes.
+echo getFreeMem()     # Total reserved RAM by GC, not used by objects, bytes.
