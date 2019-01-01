4cee93957f6f46d7b94468e8e2bedb82f53dca76
juancarlospaco <juancarlospaco@gmail.com>
2019-01-01 06:43:44 -0300

    Add Documentation for Tensors

--- ejemplos/avanzado/tensorflow/hello_tensor.nim
+++ ejemplos/avanzado/tensorflow/hello_tensor.nim
@@ -6,10 +6,12 @@ let mytensor = [
   ].toTensor
 
 
-echo mytensor.rank     # 2
-echo mytensor.shape    # [2, 3]
-echo mytensor.strides  # [3, 1]
-echo mytensor.offset   # 0
-echo mytensor          # Tensor[int] of shape [2, 3] of type "int" on backend "CPU".
+echo mytensor is Tensor # true
+echo mytensor.sizeOf    # 144
+echo mytensor.rank      # 2
+echo mytensor.shape     # [2, 3]
+echo mytensor.strides   # [3, 1]
+echo mytensor.offset    # 0
+echo mytensor           # Tensor[int] of shape [2, 3] of type "int" on backend "CPU".
 # |1      2       3|
 # |4      5       6|
